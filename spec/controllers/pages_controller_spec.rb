8456efb776c8955fef30645dd46440e500302128
Gabriel <gmoreno@Gabe-MacBook-Pro.local>
2012-06-26 13:32:43 -0700

    Added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
+
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      # puts response.body
+      response.should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+    
+    it "should have a non-blank body" do
+      get 'home'
+      # =~ # <== this is called the 'match' operator
+      response.body.should_not =~ /<body>\s*<\/body>/ 
     end
   end
 
@@ -14,6 +28,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      # puts response.body
+      response.should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
 
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      # puts response.body
+      response.should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
 end
