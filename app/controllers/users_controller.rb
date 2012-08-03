d58bf57c8a0c02bf583154117bc4d517d49e7eb3
Gabriel <gmoreno@Gabe-MacBook-Pro.local>
2012-08-03 10:42:12 -0700

    finished user signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,16 @@ class UsersController < ApplicationController
     @user  = User.new #initializes a raw User object
     @title = "Sign up"
   end
+  
+  def create
+    # raise params[:user].inspect  #use this line to show the params data instead of submitting
+    @user = User.new(params[:user]) #initialize a User object with the params retrieved from the POST request
+    if @user.save 
+      redirect_to @user, # same as 'redirect_to user_path(@user)'
+                  :flash => { :success => "Welcome to the Sample App!" }
+    else
+      @title = "Sign up"
+      render 'new'      
+    end
+  end
 end
